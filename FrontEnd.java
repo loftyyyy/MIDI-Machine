a5f8543cfdb89f2b8f0500881d447904280e8571
loftyyyy <lofty08.business@gmail.com>
2024-05-31 22:25:20 +0800

    hi

--- FrontEnd.java
+++ FrontEnd.java
@@ -1,14 +1,41 @@
 import javax.swing.*;
-public class FrontEnd {
+import javax.swing.plaf.basic.BasicOptionPaneUI;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+
+public class FrontEnd implements ActionListener{
+
+    protected JFrame frame;
+    protected JButton button;
+    protected Graphics graphics;
+
+
     public static void main(String[] args){
+        FrontEnd obj = new FrontEnd();
+        obj.window();
 
-        JFrame frame = new JFrame();
 
-        JButton button = new JButton();
-//        button.setSize(20,20);
+    }
+    public void window(){
+
+        frame = new JFrame();
+        button = new JButton("Hello");
+        button.addActionListener( this);
         frame.getContentPane().add(button);
         frame.setSize(300,300);
         frame.setVisible(true);
+
+
+
+
+
+    }
+
+    public void actionPerformed(ActionEvent e){
+        button.setText("bitch");
+
 
     }
 }
